fa8cc0823d6d1ce3e6198a9792ca436302ebc642
Jack Humphrey <jack.humphrey@mssm.edu>
2020-11-10 14:16:43 -0500

    added COLOC scripts

---
+++ COLOC/scripts/merge_COLOC_summary.R
@@ -0,0 +1,75 @@
+# merge all COLOC results
+# subset to all results with H4 >0.5
+library(tidyverse)
+library(LDlinkR)
+library(optparse)
+option_list <- list(
+    make_option(c('-i', '--inFolder' ), help='The full path to the folder that contains the COLOC results', default = "")
+)
+
+option.parser <- OptionParser(option_list=option_list)
+opt <- parse_args(option.parser)
+
+inFolder <- opt$inFolder
+
+outFile <- paste0(inFolder, "all_COLOC_summary_results.tsv.gz")
+message(" * writing to ", outFile)
+
+all_files <- list.files(inFolder, pattern = "_COLOC_summary_level.tsv", recursive = TRUE, full.names = TRUE )
+
+names(all_files) <- all_files
+
+all_res <- purrr::map_df(all_files, ~{
+    read_tsv(.x) %>% 
+    mutate(locus = as.character(locus))
+})
+
+
+# match in disease from GWAS name
+gwas_key <- 
+tribble(
+  ~GWAS, ~disease,
+    "Ripke_2014", "SCZ",
+    "Wray_2018", "MDD",
+    "IMSGC_2019", "MS",  
+    "Stahl_2019", "BPD",
+    "Daner_2020", "BPD",
+    "Nalls23andMe_2019", "PD",
+    "Lambert_2013", "AD",
+    "Marioni_2018", "AD",
+    "Jansen_2018",  "AD",
+    "Kunkle_2019",  "AD",
+    "Nicolas_2018", "ALS",
+)
+
+all_res <- left_join(all_res, gwas_key, by = "GWAS")
+
+all_res <- select(all_res, GWAS, disease, QTL,  everything() )
+
+# deal with gene
+# sQTLs include junction too
+#all_res$geneid <- map_chr(str_split(all_res$geneid, ":"), ~{ .x[ length(.x) ] })
+all_res$geneid <- str_split_fixed(all_res$geneid, "\\.", 2)[,1]
+
+gene_meta <- read_tsv("/sc/hydra/projects/ad-omics/data/references/hg38_reference/GENCODE/gencode.v30.tx2gene.tsv") %>% 
+    janitor::clean_names() %>%
+    select(genename, geneid) %>% distinct()
+
+# remove tags
+gene_meta$geneid <- str_split_fixed(gene_meta$geneid, "\\.", 2)[,1]
+
+# match on gene symbols
+all_res$genename <- gene_meta$genename[ match(all_res$geneid, gene_meta$geneid) ]
+
+# coalesce - if no gene symbol found use ID
+all_res$QTL_Gene <- coalesce(all_res$genename, all_res$geneid)
+
+# add geneid back to make sure
+all_res$QTL_Ensembl <- gene_meta$geneid[match(all_res$QTL_Gene, gene_meta$genename)] 
+
+all_res$type <- ifelse( grepl("sQTL", all_res$QTL), "sQTL", "eQTL" )
+
+all_res <- select(all_res, disease, GWAS, locus, starts_with("GWAS"), QTL, type, starts_with("QTL"), nsnps, starts_with("PP") )
+
+# write out
+write_tsv(all_res, outFile)
--- COLOC/scripts/prepare_gwas_qtls.R
+++ COLOC/scripts/prepare_gwas_qtls.R
@@ -11,7 +11,46 @@ options(echo=TRUE)
 # we're only interested in GWAS-QTL pairs that colocalise at PP4 > 0.5
 
 library(tidyverse)
-library(echolocatoR)
+#library(echolocatoR)
+library(optparse)
+
+
+#inputfile <- "Microglia_all_regions_Kunkle_2019_COLOC.RData"
+#outFolder <- "Microglia_all_regions_Kunkle_2019/"
+#QTL <- "Microglia_all_regions"
+#GWAS <- "Kunkle_2019"
+
+
+option_list <- list(
+    make_option(c('--inFile'), help='', default = "example"),
+    make_option(c('--outFolder'), help='', default = "example"),
+    make_option(c('-g', '--GWAS'), help='', default = "Kunkle_2019"),
+    make_option(c('-q','--QTL'), help='', default = "Microglia_all_regions") 
+)
+
+option.parser <- OptionParser(option_list=option_list)
+opt <- parse_args(option.parser)
+
+inFile <- opt$inFile
+outFolder <- opt$outFolder
+GWAS <- opt$GWAS
+QTL <- opt$QTL
+
+
+dataCode <- paste0(QTL, "_", GWAS)
+#outFolder <- file.path(outFolder, dataCode)
+
+outFile <-  paste0(outFolder, "/", dataCode,"_coloc_results.snp.1kgp3_ld.tsv.gz") 
+
+
+# GWAS 
+gwas_folder <- "/sc/arion/projects/pd-omics/brian/Fine_Mapping/Data/GWAS/"
+stopifnot(dir.exists(gwas_folder) )
+
+available_gwas <- list.files(path = gwas_folder, include.dirs=TRUE)
+
+stopifnot(GWAS %in% available_gwas)
+
 
 # replace gene IDs with gene names
 gene_meta <- readr::read_tsv("/sc/arion/projects/ad-omics/data/references/hg38_reference/GENCODE/gencode.v30.tx2gene.tsv") %>%
@@ -52,42 +91,24 @@ map_gene <- function(locus, tag = FALSE){
 
 # for each gene, prepare data for echoR
 
-#inputfile <- "Microglia_all_regions_Kunkle_2019_COLOC.RData"
-#outFolder <- "/hpc/users/humphj04/pipelines/downstream-QTL/Fine_Mapping/Kunkle_Microglia_all_regions"
-
-# MyND monocytes - larger sample size = better fine-mapping?
-inputfile <- "Microglia_all_regions_Nalls23andMe_2019_COLOC.RData"
-#inputfile <- "Monocytes_MyND_Kunkle_2019_COLOC.RData"
-outFolder <- "/hpc/users/humphj04/pipelines/downstream-QTL/Fine_Mapping/Microglia_all_regions_Nalls23andMe_2019/"
-
-inputfile <- "Microglia_all_regions_Kunkle_2019_COLOC.RData"
-outFolder <- "Microglia_all_regions_Kunkle_2019/"
-GWAS <- "Kunkle_2019"
-
-# microglia young
-#inputfile <- "Microglia_Young_Kunkle_2019_COLOC.RData"
-#outFolder <- "/hpc/users/humphj04/pipelines/downstream-QTL/Fine_Mapping/Kunkle_Microglia_Young/"
-
-
 if(!dir.exists(outFolder) ){
-    dir.create(outFolder)
+    dir.create(outFolder, recursive = TRUE)
 }
 
 # name is all_obj
-load(inputfile)
+load(inFile)
 
 # extract colocalized genes
-prepped <- map(all_obj, prep_locus)
+prepped <- map(all_obj, prep_locus) %>% purrr::compact()
 
 # convert gene id to gene name
-prepped <- map(prepped, map_gene)
+# drop for now - breaks on sQTLs
+#prepped <- map(prepped, map_gene)
 
-# just use BIN1 for now
-#prepped <- list(prepped$BIN1)
-#prepped <- prepped[1:10]
+save(prepped, file =  "test.RData")
 
 # use Brian's function to write objects out as echoR input dataframes
-merge_coloc_results(prepped, results_level = c("snp"), save_path = outFolder)
+echolocatoR::merge_coloc_results(prepped, results_level = c("snp"), save_path = outFolder)
 
 print("conversion to echoR completed")
 
@@ -96,43 +117,37 @@ print("conversion to echoR completed")
 merged_loci <- read_tsv( paste0(outFolder, "/merged_coloc_results.snp.tsv.gz"))
 
 ## Harmonise locus names with Brian's fine-mapping
-
-## Rename AD Loci
 refFolder <- "/sc/arion/projects/ad-omics/data/references/GWAS/"
 
+## AD - Rename Loci to be standard
+if( GWAS %in% c("Lambert_2013", "Marioni_2018", "Jansen_2018", "Kunkle_2019") ){
 ad_consensus_loci <- readxl::read_excel(paste0(refFolder,"AD_GWAS_consensus_loci_names.xlsx") )
 
 merged_loci <- left_join(merged_loci, ad_consensus_loci, by = c("Locus" = "consensus_locus_name")) 
-all_res$Locus <- coalesce(all_res$old_locus_name, all_res$Locus)
+merged_loci$Locus <- coalesce(merged_loci$old_locus_name, merged_loci$Locus)
+}
 
 ## MS - Brian used different locus names in MS for fine-mapping, use a key
+if( GWAS == "IMSGC_2019"){
 ms_locus_key <- read_tsv(paste0(refFolder,"MS_GWAS_locus_key.txt"))
 
 merged_loci$Locus_Brian <- ms_locus_key$Locus_Brian[ match(merged_loci$Locus, ms_locus_key$Locus)]
 merged_loci$Locus <- coalesce(merged_loci$Locus_Brian, merged_loci$Locus)
 merged_loci$Locus_Brian <- NULL
+}
 
+if( GWAS == "Stahl_2019"){
 # Stahl - remove "*" and gsub "," to "-"
 merged_loci$Locus <- gsub("\\*", "", gsub(",", "-", merged_loci$Locus))
+}
 
+if( GWAS == "Nalls23andMe_2019"){
 # Nalls PD - 5 loci were skipped by Brian because they didn't pass QC
 merged_loci <- filter(merged_loci, !Locus %in% c("DDRGK1", "SEMA4A", "GXYLT1", "FGD4", "ZNF608"))
-
-
-gwas_folder <- "/sc/arion/projects/pd-omics/brian/Fine_Mapping/Data/GWAS/"
-stopifnot(dir.exists(gwas_folder) )
-
-available_gwas <- list.files(path = gwas_folder, include.dirs=TRUE)
-
-stopifnot(GWAS %in% available_gwas)
+}
 
 # split merged loci back into individual loci
 all_loci_split <- split(merged_loci, merged_loci$Locus)
-
-# for each locus find the lead QTL and GWAS SNPs
-# extract LD for each SNP with both types of SNP
-#.y <- all_loci_split$MED12L
-#.x <- "MED12L"
 
 all_loci_ld <- map2_df(.y = all_loci_split, .x= names(all_loci_split), ~{
     print(paste0(' * adding LD for Locus: ', .x))
@@ -142,27 +157,49 @@ all_loci_ld <- map2_df(.y = all_loci_split, .x= names(all_loci_split), ~{
     
     ld_file <- file.path(gwas_folder, GWAS,.x, "LD", paste0( .x , ".1KGphase3_LD.RDS")  )
     stopifnot(file.exists(ld_file))
-
+    
+    print(ld_file)
     ld_matrix <- readRDS(ld_file)
-
+    
     lead_gwas_snp <- arrange(.y,  gwas.pvalues, desc(abs(gwas.beta)) ) %>% head(1) %>% pull(snp)
     lead_qtl_snp <-  arrange(.y,  qtl.pvalues, desc(abs(qtl.beta)) ) %>% head(1) %>% pull(snp)
+    
+    print(paste0(" * lead QTL SNP: ", lead_qtl_snp) )
+    print(paste0(" * lead GWAS SNP: ", lead_gwas_snp) )
+    
+    ld_res <- .y
+    
+    # stupid error handling - if either SNP is not in the LD matrix
+    if(lead_qtl_snp %in% colnames(ld_matrix) ){
+        lead_qtl_ld <- enframe(ld_matrix[,lead_qtl_snp])
+        names(lead_qtl_ld) <- c("snp", "lead_qtl_ld")
+        lead_qtl_ld$snp <- row.names(ld_matrix)
+        
+        ld_res <- left_join(.y, lead_qtl_ld, by = "snp")
+        ld_res$lead_qtl_ld <- ld_res$lead_qtl_ld^2
 
-    lead_qtl_ld <- enframe(ld_matrix[,lead_qtl_snp])
-    names(lead_qtl_ld) <- c("snp", "lead_qtl_ld")
+    }else{
+        ld_res$lead_qtl_snp <- NA
+    }
+    
+    if(lead_gwas_snp %in% colnames(ld_matrix) ){
+        lead_gwas_ld <- enframe(ld_matrix[,lead_gwas_snp])
+        names(lead_gwas_ld) <- c("snp", "lead_gwas_ld")
+        lead_gwas_ld$snp <- row.names(ld_matrix)    
+        
+        ld_res <- left_join(.y, lead_gwas_ld, by = "snp")
+        ld_res$lead_gwas_ld <- ld_res$lead_gwas_ld^2
 
-    lead_gwas_ld <- enframe(ld_matrix[,lead_gwas_snp])
-    names(lead_gwas_ld) <- c("snp", "lead_gwas_ld")
-
-    ld_res <- left_join(.y, lead_gwas_ld, by = "snp") %>% left_join(lead_qtl_ld, by = "snp")
+    }else{
+        ld_res$lead_gwas_snp <- NA
+    }
+  e 
+    #ld_res <- left_join(.y, lead_gwas_ld, by = "snp") %>% left_join(lead_qtl_ld, by = "snp")
     
     # the LD matrices store R, not R^2. So square them!
-    ld_res$lead_qtl_ld <- ld_res$lead_qtl_ld^2
-    ld_res$lead_gwas_ld <- ld_res$lead_gwas_ld^2
-
+        
     return(ld_res)
 
 })
 
-write_tsv(all_loci_ld, path = paste0(outFolder, GWAS, "_coloc_results.snp.1kgp3_ld.tsv.gz") )
-
+write_tsv(all_loci_ld, path = outFile)
